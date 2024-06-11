b4bcd7d35b887cac2c9d1dc5c638c1700eaac55b
quintenvandamme <quintenvandamme@proton.me>
2024-06-11 20:26:51 +0200

    chore: Generate libflatpak.dart bindings and update README.md

--- bin/flatpak.dart
+++ bin/flatpak.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'package:ffi/ffi.dart';
 import 'package:flatpak/src/libflatpak.dart';
 
 class Flatpak {
@@ -15,9 +16,37 @@ class Flatpak {
     int micro = FLATPAK_MICRO_VERSION;
     return '$major.$minor.$micro';
   }
+
+  int getInstances() {
+    Pointer<GPtrArray> instances = libFlatpak.flatpak_instance_get_all();
+    return instances.ref.len;
+  }
+
+  List<String> getSupportedArches() {
+    Pointer<Pointer<Char>> supportedArches =
+        libFlatpak.flatpak_get_supported_arches();
+
+    List<String> supportedArchesList = [];
+    int index = 0;
+
+    while (true) {
+      Pointer<Char> archPtr = supportedArches.elementAt(index).value;
+      if (archPtr == nullptr) {
+        break;
+      }
+      String arch = archPtr.cast<Utf8>().toDartString();
+      supportedArchesList.add(arch);
+      index++;
+    }
+
+    malloc.free(supportedArches);
+
+    return supportedArchesList;
+  }
 }
 
 void main() {
   Flatpak flatpak = Flatpak();
   print("Flatpak version: ${flatpak.version()}");
+  print("Supported arches: ${flatpak.getSupportedArches()}");
 }
